f0e52845223d35e5490529cf6d69339ff0cfb708
AvaTuttle <Travis.Tuttle@sdgku.edu>
2024-10-10 11:55:16 -0700

    assignmment 1

--- main-index/scripts/register.js
+++ main-index/scripts/register.js
@@ -4,31 +4,31 @@ let pets =[];
 let pet1 = {
     name:"scooby",
     age:4,
-    gender:"Male"
-
+    gender:"Male",
+    Service:false,
+    Breed:"Choocolate Labrador"
 }
 let pet2 = {
     name:"Miley",
     age:7,
-    gender:"Female"
-
+    gender:"Female",
+    service:true,
+    Breed:"Pomeranian"
 }
 
 let pet3 = {
     name:"Sir Grunge McAllister",
     age:12,
-    gender:"Male"
-
+    gender:"Male",
+    Service:false,
+    Breed:"Dachshound"
 }
 
 pets.push(pet1, pet2, pet3)
 
 function displayNames(){
-    //display pet neames
-    console.log(pets[0].name);
-    console.log(pets[1].name);
-    console.log(pets[2].name);
-    //hint: use a for loop and travel the array
-    //hint: length the amount of elements on the array
-    //write on HTML, not console
+    document.body.innerHTML = "";
+    for (let i = 0; i < pets.length; i++) {
+        document.write(pets[i].name + "<br>");        
+    }
 }
