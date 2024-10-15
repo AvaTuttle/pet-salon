ff71b636a3dd7ee3607a39bf8c649541567e3d24
AvaTuttle <Travis.Tuttle@sdgku.edu>
2024-10-14 17:18:06 -0700

    tables added and cards hidden. used bootstrap

---
+++ main-index/scripts/display.js
@@ -0,0 +1,42 @@
+function displayCards(){
+    let card="";
+    for(let i=0; i<pets.length; i++){
+        let pet = pets[i];
+        card+=`
+        <div class="pet ${pet.service}">
+            <p>Type: ${pet.type}</p>
+            <p>Breed: ${pet.breed}</p>
+            <p>Name: ${pet.name}</p>
+            <p>Age: ${pet.age}</p>
+            <p>Gender: ${pet.gender}</p>
+            <p>Service: ${pet.service}</p>
+        </div>
+        `;
+        console.log(card);
+    }
+    document.getElementById("pets").innerHTML=card;
+}
+
+
+function displayTotals(){
+    document.getElementById("total").innerHTML=pets.length;
+}
+
+function displayTable() {
+    let row = "";
+    for (let i = 0; i < pets.length; i++) {
+        let pet = pets[i];
+        row += `
+        <tr>
+            <td>${pet.type}</td>
+            <td>${pet.breed}</td>
+            <td>${pet.name}</td>
+            <td>${pet.age}</td>
+            <td>${pet.gender}</td>
+            <td>${pet.service}</td>
+        </tr>
+        `;
+    }
+    document.getElementById("petTableBody").innerHTML =row; 
+}
+
--- main-index/scripts/register.js
+++ main-index/scripts/register.js
@@ -20,13 +20,16 @@ function register(){
     let newPet = new Pet (inputType,inputBreed,inputName,inputAge,inputGender,inputServices)
     console.log(newPet);
     pets.push(newPet);
+    displayTotals();
+    displayCards(); 
+    displayTable();//displayTable()
 }
 
 function init(){
     console.log("init");
-    let pet1 = new Pet("dog", "scooby", 9, "male", "Grooming")
-    let pet2 = new Pet("dog", "Miley", 4, "female", "nails")
-    let pet3 = new Pet("cat", "Sir Grunge McAllister", 13, "male", "bathing")
+    let pet1 = new Pet("dog", "dalmation","scooby", 9, "male", "grooming")
+    let pet2 = new Pet("dog", "Poodle", "Miley", 4, "female", "nails")
+    let pet3 = new Pet("cat", "domestic Long Hair", "Sir Grunge McAllister", 13, "male", "bathing")
     pets.push(pet1,pet2,pet3)
     console.log(pets)
 
